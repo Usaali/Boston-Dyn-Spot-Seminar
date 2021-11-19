45934ce8cdc7bb87a45b4b91d4d1312fcc188ab3
Usama Ali <usama.ali@robotics-erlangen.de>
2021-11-19 13:45:23 +0100

    use a segmentation based approach to detect if fingers are open or closed

--- hand_detection.py
+++ hand_detection.py
@@ -3,6 +3,46 @@ import mediapipe as mp
 import imutils
 import time
 
+def bg_avg(img, bg, weight):
+    """ This function takes an image and adds its weight to the averaged background variable
+
+    Args:
+        img: the image that should be processed
+        weight: the weight with which the image impacts the background
+    """
+    #initialize the background on first run
+    if bg is None:
+        bg = img.copy().astype("float")
+        return bg
+
+    #get the average weight of the image
+    cv2.accumulateWeighted(img,bg,weight)
+    return bg    
+
+#segments a region from the background
+def segment(img, bg, threshold=25):
+    """ This function takes an image, calculates the difference from the background variable and returns a contour of the difference
+
+    Args:
+        img: The image that is segmented
+        threshold: The threshold that is used to segment the image. Defaults to 25.
+
+    Returns:
+        A tuple containing the thresholded image and the contour
+    """
+    #calculate absolute difference between input and background
+    diff = cv2.absdiff(bg.astype("uint8"),img)
+    #threshold the result to get a clearer image
+    thresholded = cv2.threshold(diff,threshold,255,cv2.THRESH_BINARY)[1]
+    #get contours of the difference
+    (cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if (len(cnts))  == 0: #no contour was found (something went wrong)
+        return
+    else:
+        segmented = max(cnts, key=cv2.contourArea)
+        return (thresholded, segmented)
+
 def findHand(frame, handDetector):
     """ This function uses the mediapipe to detect a hand
 
@@ -40,34 +80,38 @@ def findHand(frame, handDetector):
     results = handDetector.process(imgRGB) #run the image through the mediapipe detector
 
     if results.multi_hand_landmarks is not None:
-        mpDraw.draw_landmarks(frame,results.multi_hand_landmarks[0],mpHands.HAND_CONNECTIONS) #draw the hand connections onto the frame. NOTE: comment this out if it is unwanted
+        #mpDraw.draw_landmarks(frame,results.multi_hand_landmarks[0],mpHands.HAND_CONNECTIONS) #draw the hand connections onto the frame. NOTE: comment this out if it is unwanted
         return results.multi_hand_landmarks[0].landmark
     else:
         return None
 
-def contourHand(img):
+def contourHand(img, offset):
     """ This function tries to process an image and find the contours of the hand
 
     Args:
         img: The image of the hand (and just the hand)
-
+        offset: The offset for the contour
     Returns:
         A contour array
     """
     if(img is None):
         return
+     
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert image to gray for better thresholding
-    gray = cv2.GaussianBlur(gray,(5,5),0) #add blur to reduce noise impact
-    thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1] #threshold the image. NOTE: the threshold value is entirely experimental and varies with different lighting
+    gray = cv2.GaussianBlur(gray,(11,11),0) #add blur to reduce noise impact
+    (T, thresh) = cv2.threshold(gray, 0, 200, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU) #use OTSU thresholding method to dynamically determine a threshold value
     thresh = cv2.erode(thresh, None, iterations=2)  #add erosion effect to reduce noise impact
     thresh = cv2.dilate(thresh, None, iterations=2) #add dilation effect to reduce noise impact
     cv2.imshow("Tresholded",thresh) #show the tresholded image
     
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) #find the contours
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE, offset=offset) #find the contours
     cnts = imutils.grab_contours(cnts)
-    c = max(cnts, key=cv2.contourArea)
-    cv2.drawContours(img, [c], -1, (0, 255, 255), 2) #draw the contours onto the image
-    return c
+    if (len(cnts) != 0):
+        c = max(cnts, key=cv2.contourArea)
+        #cv2.drawContours(img, [c], -1, (0, 255, 255), 2) #draw the contours onto the image
+        return c
+    else:
+        return None
 
 def contourOnLine(x1,y1,x2,y2,c):
     """ This is a helper function to detect if a contour cuts a line between two points p1 and p2
@@ -88,10 +132,27 @@ def contourOnLine(x1,y1,x2,y2,c):
     m = (y2-y1)/(x2-x1) 
     for p in c:
         x3,y3=p[0]
-        if min(x1,x2)<=x3<=max(x1,x2) and min(y1,y2)<=y3<=max(y1,y2):
-            if y3-y1 == m * (x3-x1):
+        #print("Point is: "+str(x3)+","+str(y3))
+        if isBetween(x1,y1,x2,y2,x3,y3):
                 return True
     return False
+
+def isBetween(x1, y1, x2, y2, x3, y3):
+    crossproduct = (y3 - y1) * (x2 - x1) - (x3 - x1) * (y2 - y1)
+
+    # compare versus epsilon for floating point values, or != 0 if using integers
+    if abs(crossproduct) != 0:
+        return False
+
+    dotproduct = (x3 - x1) * (x2 - x1) + (y3 - y1)*(y2 - y1)
+    if dotproduct < 0:
+        return False
+
+    squaredlengthba = (x2 - x1)*(x2 - x1) + (y2 - y2)*(y2 - y1)
+    if dotproduct > squaredlengthba:
+        return False
+
+    return True    
 
 def fingersClosed(frame, landmarks, contour):
     """ This function tries to determine if the fingers of a hand are closed or not
@@ -118,12 +179,10 @@ def fingersClosed(frame, landmarks, contour):
 
 mpDraw = mp.solutions.drawing_utils
 mpHands = mp.solutions.hands
-
 if __name__ == "__main__":
     cam = cv2.VideoCapture(0) #set default video input as camera
-
     #initialize the detector in video mode, with a maximum of one hand
-    handDetector =  mpHands.Hands(static_image_mode=False,
+    handDetector =  mpHands.Hands(static_image_mode=True,
                           max_num_hands=1,
                           min_detection_confidence=0.5,
                           min_tracking_confidence=0.5)
@@ -135,28 +194,45 @@ if __name__ == "__main__":
     while(True):
         (grabbed, frame) = cam.read() #get live image from camera
         frame = cv2.flip(frame, 1) #flip the image to get mirrored view
-
+        h, w, c = frame.shape
         landmarks = findHand(frame, handDetector) #let mediapipe detect the hand
         
         if landmarks is not None:
             minX, minY = frame.shape[:2]
             maxX = 0
             maxY = 0
+            points = [5,7,17,19]
             for id, lm in enumerate(landmarks):
-                h, w, c = frame.shape
-                cx,cy = int(lm.x*w), int(lm.y*h) #get the absolute landmark coordinates
-                #calculate min and max values for x and y to put a rectangle around the hand
-                minX = min(minX,cx)
-                maxX = max(maxX,cx)
-                minY = min(minY,cy)
-                maxY = max(maxY,cy)
-                cv2.circle(frame, (cx,cy),3,(255,0,0),cv2.FILLED) #draw on every landmark
-            cv2.rectangle(frame,(minX,minY),(maxX,maxY),(255,255,0),2) #put a rectangle around the hand
+                if id in points:
+                    cx,cy = int(lm.x*w), int(lm.y*h) #get the absolute landmark coordinates
+                    #calculate min and max values for x and y to put a rectangle around the hand
+                    minX = min(minX,cx)
+                    maxX = max(maxX,cx)
+                    minY = min(minY,cy)
+                    maxY = max(maxY,cy)
+                    #cv2.circle(frame, (cx,cy),3,(255,0,0),cv2.FILLED) #draw on every landmark
+            #cv2.rectangle(frame,(minX,minY),(maxX,maxY),(255,255,0),2) #put a rectangle around the hand
+            
             margin = 20
-            handPic = frame[minY-margin:maxY+margin,minX-margin:maxX+margin] #take an image from the hand with a margin because the landmark is not on the edge of a finger
-            contour = contourHand(handPic)
+            handPic = frame[max(minY-margin,0):min(maxY+margin,h),max(minX-margin,0):min(maxX+margin,w)] #take an image from the hand with a margin because the landmark is not on the edge of a finger
+            
+            contour = contourHand(handPic, (max(minX-margin,0),max(minY-margin,0)))
             wrist = landmarks[0] #grab wrist position for placing text there 
-            cv2.putText(frame,str(fingersClosed(frame,landmarks,contour)), (int(wrist.x*w),int(wrist.y*h)), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 3) #check if fingers are closed and write the result onto the wrist
+            if contour is not None:
+                hull = cv2.convexHull(contour, returnPoints=False)
+                hullPts = cv2.convexHull(contour)
+                cv2.drawContours(frame, [contour], -1, (0, 255, 255), 2) #draw the contours onto the image
+                cv2.drawContours(frame, [hullPts], -1, (255, 0, 255), 2) #draw the contours onto the image
+                defects = cv2.convexityDefects(contour,hull)
+                for i in range(defects.shape[0]):
+                    s,e,f,d = defects[i,0]
+                    start = tuple(contour[s][0])
+                    end = tuple(contour[e][0])
+                    far = tuple(contour[f][0])
+                    cv2.line(frame,start,end,[0,255,0],2)
+                    cv2.circle(frame,far,5,[0,0,255],-1)
+
+                cv2.putText(frame,str(fingersClosed(frame,landmarks,contour)), (int(wrist.x*w),int(wrist.y*h)), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 3) #check if fingers are closed and write the result onto the wrist
             cv2.imshow("Hand", handPic) #display the picture of the hand with contour
         #calculate fps
         cTime = time.time()
@@ -165,7 +241,8 @@ if __name__ == "__main__":
         cv2.putText(frame,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3) #print fps on frame
 
         cv2.imshow("Video", frame) #display live feed
-
+        #input()
+        #exit()
         input = cv2.waitKey(1) & 0xFF #quit the program with q
         if input == ord("q"):
             break
